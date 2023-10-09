e2d6f283a429bc8e69b746542e5c036cf29f8e29
BrianTukei <briantukei1000@gmail.com>
2023-10-09 11:33:11 -0700

    0-positive_or_negative.c

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - postive,zero or negative
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		prinft("%d is zero\n", n);
+	return (0);
+}
+
