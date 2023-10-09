0984db8a740c77be3169a9c00b2509d0f1e01aae
BrianTukei <briantukei1000@gmail.com>
2023-10-08 17:01:27 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,9 @@
 
 int main(void)
 {
-	  char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"i;
 
-	  write(2, s, 59);
-          return (1);
+	write(2, s, 59);
+	return (1);
 }
 
