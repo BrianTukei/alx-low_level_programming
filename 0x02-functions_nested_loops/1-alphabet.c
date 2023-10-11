78e148b690f1139b2147834b6e8cf88072c01b64
BrianTukei <briantukei1000@gmail.com>
2023-10-11 11:13:14 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,12 +7,10 @@
 
 void print_alphabet(void)
 {
-	char c;
+	char ch;
 
-	for (c = "a"; c <= 'z'; c++)
-	{
-		_putchar(c);
+	for (ch = 'a'; ch <= 'z'; c++)
+		_putchar(ch);
 		_putchar('\n');
-	}
 }
 
