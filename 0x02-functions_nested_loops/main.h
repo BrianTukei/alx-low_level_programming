6cca5a09325ab74ca44ff7d6678de5622f964221
BrianTukei <briantukei1000@gmail.com>
2023-10-11 07:42:22 -0700

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,20 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stddef.h>
-#include <stdio.h>
 
 int _putchar(char c);
-print_alphabet(void);
-print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-print_times_table(int n);
 
 #endif
