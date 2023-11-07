45ea10585ac35f37bfcbf806b93a3cedfae3c4ed
BrianTukei <briantukei1000@gmail.com>
2023-11-06 20:26:45 -0800

    101-strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -15,6 +15,7 @@ char **strtow(char *str);
 int word_len(char *str)
 {
 	int index = 0, len = 0;
+
 	while (*(str + index) && *(str + index) != ' ')
 	{
 		len++;
@@ -32,6 +33,7 @@ int word_len(char *str)
 int count_words(char *str)
 {
 	int index = 0, words = 0, len = 0;
+
 	for (index = 0; *(str + index); index++)
 		len++;
 	for (index = 0; index < len; index++)
@@ -54,15 +56,16 @@ int count_words(char *str)
 char **strtow(char *str)
 {
 	char **strings;
+
 	int index = 0, words, w, letters, l;
-	
+
 	if (str == NULL || str[0] == '\0')
 		return (NULL);
 
 	words = count_words(str);
 	if (words == 0)
 		return (NULL);
-	
+
 	strings = malloc(sizeof(char *) * (words + 1));
 	if (strings == NULL)
 		return (NULL);
