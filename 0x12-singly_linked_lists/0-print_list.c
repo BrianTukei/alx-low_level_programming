f22616bb13377db8f760a6262cdfdca026b50b37
BrianTukei <briantukei1000@gmail.com>
2023-11-16 13:54:00 -0800

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,12 +10,12 @@
  */
 size_t print_list(const list_t *h)
 {
-	size _t count = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[o] (nil)\n");
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
