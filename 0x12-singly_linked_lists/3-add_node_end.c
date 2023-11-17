5fb584a77b2d0764a98e400cb29a9e932be25b55
BrianTukei <briantukei1000@gmail.com>
2023-11-17 07:51:18 -0800

    ANGEL

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -33,8 +33,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		{
 			temp = *head;
 			while (temp->next)
-				while (temp->next)
-					temp = temp->next;
+				temp = temp->next;
 			temp->next = new_list;
 			return (temp);
 		}
