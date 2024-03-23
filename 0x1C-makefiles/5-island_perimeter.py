77efb62415b70a579996a5b372fe35e5225a7aa9
BrianTukei <briantukei1000@gmail.com>
2024-03-22 17:32:45 -0700

    Makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,26 +1,36 @@
 #!/usr/bin/python3
-"""5-island_perimeter module"""
+"""
+Module Island Perimeter
+"""
 
 
 def island_perimeter(grid):
-    """Returns the perimeter of the island described in grid"""
-    c = 0
-    length = len(grid) - 1
-    width = len(grid[0]) - 1
+    """ Calculate perimeter of grid where "1" is found"""
+    p = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == 1:
+                if row == 0 or grid[row - 1][col] == 0:
+                    p += 1  # top
+                    if row == (len(grid) - 1) or grid[row + 1][col] == 0:
+                        p += 1  # bottom
+                        if col == 0 or grid[row][col - 1] == 0:
+                            p += 1  # left
+                            if col == (len(grid[0]) - 1) or grid[row][col + 1] == 0:
+                                p += 1  # right
+                                return p
 
-    for i, r in enumerate(grid):
-        for j, n in enumerate(r):
-            if n == 1:
-                if i == 0 or grid[i - 1][j] != 1:
-                    c += 1
 
-                    if j == 0 or grid[i][j - 1] != 1:
-                        c += 1
-
-                        if j == width or grid[i][j + 1] != 1:
-                            c += 1
-
-                            if i == length or grid[i + 1][j] != 1:
-                                c += 1
-
-                                return c
+                            #    Method for rectangular island only, no odd shapes
+                            #    maxWidth = 0
+                            #    length = 0
+                            #    for i in range(len(grid)):
+                            #        width = 0
+                            #        for j in range(len(grid[0])):
+                            #            if grid[i][j] == 1:
+                            #                width += 1
+                            #        if width:
+                            #            length += 1
+                            #        if width > maxWidth:
+                            #            maxWidth = width
+                            #    return ((maxWidth + length) * 2)
